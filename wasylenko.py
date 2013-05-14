00e9f3aef6c15529262ccfb37cdbd74af6374f01
Tom S. Bertalan <tom@tombertalan.com>
2013-05-13 20:26:22 -0400

    copied in actual (modified from previous emails) code
    code for integrating the system in time, that is

--- wasylenko.py
+++ wasylenko.py
@@ -1,1 +1,219 @@
-/home/tsbertalan/Documents/School/13spring2013/448cbe448/hetNet/wasylenko.py
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+
+def vecAvg(vec):
+    '''Adds up all the values in a numpy array, and divide by the count of values.
+    Assumes that vec is a numpy ndarray or numpy matrix.'''
+    return np.sum(vec) / vec.size
+
+def doint(initial, dxdt, tmin=0, tmax=800, nstep=1e5):
+    '''Integrate a given differential system forward from given initial conditions.
+    Returns X and T:
+     in X, the first index is across times, and the second index is across state variables.
+     T has only one index; across times.
+    It's possible that Scipy is using adaptive time stepping, in which case the (linearly
+     spaced) values of T will be inaccurate. Use with caution.'''
+    def dxdtextra(X, T=0):
+        return dxdt(X, T=T)
+    T = np.linspace(tmin, tmax, nstep)
+    X0 = np.array(initial)
+    X, infodict = integrate.odeint(dxdtextra, X0, T, full_output=True)
+    return (X, T)
+
+def main(N=60, omega=6, s=.5, plot=True, tmax=.125, nstep=1000, verbose=True, X0=None):
+    
+    # parameters and auxiliary functions:
+    
+       # These are intended as vector-valued functions--they take a vector,
+       #   and return a vector (i.e., a numpy ndarray or matrix).
+       #   However, they would also work just fine with scalars. Python is duck-typed.
+    def minf(v):
+        return 1 / (1 + np.exp(-(v - thm) / sigm))
+    
+    def hinf(v):
+        return 1 / (1 + np.exp(-(v - thh) / sigh))
+    
+    def sinf(v):
+        return 1 / (1 + np.exp(-(v - ths) / sigs))
+        
+    def tauinf(v):
+        return tau1 + (tau2 - tau1) / (1 + np.exp(-(v - thtau) / sigtau))
+    
+    # fixed parameters
+    ths = -20
+    sigs = 2
+    thh = -79
+    sigh = -5
+    thm = -65
+    sigm = 7.8
+    thtau = -65
+    sigtau = 4
+    tau1 = 1
+    tau2 = 80
+    
+    gLTC = .01
+    VLTC = -75
+    VsynTC = 0
+    gLRE = .2
+    VLRE = -80
+    epRE = 2
+    VsynRE = -80
+    gCa = 1
+    Cm = 1
+    
+    # bifurcation parameters
+    epTC = 1 + 2 * s
+    gTC = 0.03 + 0.07 * s
+    gRE = 0.1 + 0.2 * s
+    VCa = 120 - 30 * s
+    
+    def theSum(sinfVTC):
+        '''Used for computing the sum of inputs to each of the
+        RE neurons. Takes as argument a precomputed vector of s_inf(v_i)
+        values for each of the TC neurons' voltages, v_i.'''
+        shape = sinfVTC.shape
+        sinfVTC = list(sinfVTC)
+        N = len(sinfVTC)
+        toreturn = list(np.zeros(shape))
+        for i in range(N):
+#             print "---"
+            for k in range(-omega, omega+1):
+                index = i + k
+                if index >= N: index -= N
+#                 print "i:", i, "k:", k,  "index:", index
+                toreturn[i] += sinfVTC[index]
+        return np.array(toreturn).reshape(shape)
+    
+#     wuts = [[], [], []]
+    def diff(X, T=0):
+        '''The vector system-of-equations.
+        For the state vector X, of shape (4*N,)
+            the first  N elements are the VTC voltages,
+            the second N elements are the hTC coeffecients
+            the third  N elements are the VRE voltages, and
+            the fourth N elements are the hRE coeffecients.
+        Returns an array of shape (4*N,) of the rates-of-change of these state variables.
+        '''
+        # split the state vector into meaningful pieces:
+        VTC = X[0*N:1*N]
+        hTC = X[1*N:2*N]
+        VRE = X[2*N:3*N]
+        hRE = X[3*N:4*N]
+        sinfVTC = sinf(VTC)  # precompute s_inf(v) values for use in theSum()
+        
+        # the four differential equations:
+        dVTCdt = (-gLTC * (VTC - VLTC)
+                  -gCa * (minf(VTC)) ** 3 * hTC * (VTC - VCa)
+                  -gTC * sinf(VRE) * (VTC - VsynRE)
+                 ) / Cm
+        dhTCdt = epTC * (hinf(VTC) - hTC) / tauinf(VTC)
+        dVREdt = (-gLRE * (VRE - VLRE)
+                  -gCa * (minf(VRE)) ** 3 * hRE * (VRE - VCa)
+                  -gRE / (2*omega+1) * theSum(sinfVTC) * (VRE - VsynTC)
+                 ) / Cm
+        dhREdt = epRE * (hinf(VRE) - hRE) / tauinf(VRE)
+        # show progress
+        if verbose:
+            if not int(T) % 100:
+                print T
+        
+        # Wasylenko 2010, Sec. 3.2:
+        #  First, we started with a perturbation in the vTC from the (spatially
+        #  uniform) rest state. This generated both left- and right-traveling 
+        #  waves, but by transiently changing the boundary conditions it was 
+        #  possible to eliminate the left-travelingwave.
+        # Here is that transient modification. It's possible that I could do it better:
+        if T<600:
+            dVTCdt[0:omega] = 0
+            dhTCdt[0:omega] = 0
+            dVREdt[0:omega] = 0
+            dhREdt[0:omega] = 0
+            
+#         if 600 < T < 610:
+#             pp(X)
+            
+        return np.array(np.vstack((dVTCdt, dhTCdt, dVREdt, dhREdt))).reshape((4*N,))
+
+    # Initial Conditions (near resting point,
+    #  which was determined by poorly choosing
+    #  ICs and then measuring their mean steady-state values.)
+    if X0==None:
+        X0 = np.ones((N * 4,))
+        X0[0*N:1*N] = -49  # VTC_0
+        X0[1*N:2*N] = 0    # hTC_0
+        X0[2*N:3*N] = -78  # VRE_0
+        X0[3*N:4*N] = 0.5  # hRE_0
+        X0[omega+3] = 0    # a small perturbation in VTC_0
+    
+    # Uncomment this for krazy fun. Surprisingly, the system still settles
+    #  into a traveling-wave behavior, if the transient BC-modification is maintained long enough:
+#     X0 = np.random.random((N*4,)) * 120 - 60 
+
+    X, T = doint(X0, diff, tmin=0, tmax=tmax, nstep=nstep)
+    
+    if plot:
+        plotMultiple(X, T)
+        
+    # This is what I used to determine the "resting" conditions
+    #  (except, without the transient BC-modification):
+    if verbose:
+        print "average final VTC is", vecAvg(X[-1, 0*N:1*N])
+        print "average final hTC is", vecAvg(X[-1, 1*N:2*N])
+        print "average final VRE is", vecAvg(X[-1, 2*N:3*N])
+        print "average final hRE is", vecAvg(X[-1, 3*N:4*N])
+    return X, T
+
+def plotMultiple(X, T, s=0.5, show=False):
+    N = X.shape[1] / 4
+    print N
+    N = int(N)
+    fig = plt.figure(figsize=(11*1.5, 8.5*1.5))
+    axes = [fig.add_subplot(1, 4, i+1) for i in range(4)]
+    imgplots = range(4)
+    cmaps = ['hot', 'jet', 'cool', 'bone']
+    titles = [r"$V^{TC}$", r"$h^{TC}$", r"$V^{RE}$", r"$h^{RE}$"]
+    for i in range(4):
+        imgplots[i] = axes[i].imshow(X[:, i*N:(i+1)*N], aspect="auto", origin="lower")
+        axes[i].set_title(titles[i])
+        imgplots[i].set_interpolation('nearest')
+        imgplots[i].set_cmap(cmaps[i])
+        fig.colorbar(imgplots[i], orientation ='horizontal')
+    fig.suptitle(r"$s=%.2f$" % s)
+    fig.savefig("wasylenko_waves-s%.2f.pdf" % s)
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(1, 1, 1)
+    for i in range(N):
+        ax2.plot(X[-50:-1, i], X[-50:-1, 2*N+i], 'k')
+    ax2.set_xlabel(r"$V^{TC}$")
+    ax2.set_ylabel(r"$V^{RE}$")
+    fig2.suptitle(r"$s=%.2f$" % s)
+    fig2.savefig("phasespace-s%.2f.pdf" % s)
+    if show:
+        plt.show()
+
+def sRange():
+    '''Run the integration for several different values of s'''
+    fig4 = plt.figure()
+    ax4 = fig4.add_subplot(1, 1, 1)
+    s = list(np.arange(0, 0.5, 0.1))
+    s.extend(list(np.arange(0.5, 1, 0.02)))
+    avg = range(len(s))
+    for i, s_ in enumerate(s):
+        X, T = main(N=60, s=s_, omega=6, tmax=3000, nstep=2000)
+        avg[i] = np.sum(X[:, 0:N]) / X[:, 0:N].size
+    ax4.plot(s, avg)
+    ax4.set_xlabel(r"arc length parameter, $s$")
+    ax4.set_ylabel(r"average $V^{TC}$")
+    fig4.suptitle("$V^{TC}$ averaged across %d neurons for different values of $s$" % N)
+    fig4.savefig("averge_VTC_values_over_s.pdf")
+
+if __name__=="__main__":
+    main(N=60, omega=6, tmax=2000, s=0.6, nstep=1000)
+    # uncomment either the sRange line to run for many values of s:
+#     sRange()
+    # In that case, you might want to comment out the plt.show line.
+    plt.show()
+    
+
